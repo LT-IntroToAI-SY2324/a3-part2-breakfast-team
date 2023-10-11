d62f52084b3b144ababd9a025ff463982e2f9b75
Kristian Chappell <kchappell1@cps.edum>
2023-10-11 11:29:42 -0500

    added foundation of search

--- chatBot.py/breatfastbot.py
+++ chatBot.py/breatfastbot.py
@@ -1,3 +1,9 @@
 from foodAndIngredients import foods_db
 from match import match
 from typing import List, Tuple, Callable, Any
+
+def get_dish(foodAndIngredients: Tuple[str, List[str]]) -> str:
+    return foodAndIngredients[0]
+
+def get_ingredients(foodAndIngredients: Tuple[str, List[str]]) -> List[str]:
+    return foodAndIngredients[1]
--- chatBot.py/foodAndIngredients.py
+++ chatBot.py/foodAndIngredients.py
@@ -1,10 +1,10 @@
 from typing import List, Tuple
-foods_db: List[Tuple[str, str, int, List[str]]] = [
+foods_db: List[Tuple[str, List[str]]] = [
     (
         "Pancakes", #The Dish
         [
             "Flour", "Baking Powder", "Sugar", "Salt", "Milk", "Butter", "Eggs"
-        ]
+        ] #The Ingredients
     ),
     (
         "Ice Cream",
