0fd3329cfdb3fd4efddd55fcfa93c984f62c3611
Kristian Chappell <kchappell1@cps.edum>
2023-10-11 11:35:34 -0500

    starting to create functions

--- chatBot.py/breatfastbot.py
+++ chatBot.py/breatfastbot.py
@@ -7,3 +7,20 @@ def get_dish(foodAndIngredients: Tuple[str, List[str]]) -> str:
 
 def get_ingredients(foodAndIngredients: Tuple[str, List[str]]) -> List[str]:
     return foodAndIngredients[1]
+
+def dish_by_ingredients
+# def title_by_actor(matches: List[str]) -> List[str]:
+#     """Finds titles of all movies that the given actor was in
+
+#     Args:
+#         matches - a list of 1 string, just the actor
+
+#     Returns:
+#         a list of movie titles that the actor acted in
+#     """
+#     results = []
+#     for movie in movie_db:
+#         if matches[0] in get_actors(movie):
+#             results.append(get_title(movie))
+#     # print(results)
+#     return results
